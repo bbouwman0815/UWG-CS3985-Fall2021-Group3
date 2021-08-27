726aed97ffd9e29edbf0c4467fbfc121f99264ad
Brian Bouwman <bbouwma1@my.westga.edu>
2021-08-27 10:05:04 -0400

    Added documentation for user class. Added UI messages for null && empty user constructor

--- src/edu/westga/cs4985/clinicApp/model/User.java
+++ src/edu/westga/cs4985/clinicApp/model/User.java
@@ -1,20 +1,57 @@
 package edu.westga.cs4985.clinicApp.model;
 
+import edu.westga.cs4985.clinicApp.resources.UI;
+
+/*
+ * The User Class
+ */
 public class User {
 
 	public String username;
-	
+
 	public String password;
-	
+
+	/**
+	 * Instantiates a new user.
+	 * 
+	 * @precondition username != null && !(username.isEmpty()) && password != null
+	 *               && !(password.isEmpty())
+	 * @postcondition username = getUsername() && password = getPassword()
+	 *
+	 * @param username the username
+	 * @param password the password
+	 */
 	public User(String username, String password) {
+		if (username == null) {
+			throw new IllegalArgumentException(UI.ExceptionMessages.NULL_USERNAME);
+		}
+		if (username.isEmpty()) {
+			throw new IllegalArgumentException(UI.ExceptionMessages.EMPTY_USERNAME);
+		}
+		if (password == null) {
+			throw new IllegalArgumentException(UI.ExceptionMessages.NULL_PASSWORD);
+		}
+		if (password.isEmpty()) {
+			throw new IllegalArgumentException(UI.ExceptionMessages.EMPTY_PASSWORD);
+		}
 		this.username = username;
 		this.password = password;
 	}
-	
+
+	/**
+	 * Gets the username.
+	 *
+	 * @return the username
+	 */
 	public String getUsername() {
 		return this.username;
 	}
-	
+
+	/**
+	 * Gets the password.
+	 *
+	 * @return the password
+	 */
 	public String getPassword() {
 		return this.password;
 	}
--- src/edu/westga/cs4985/clinicApp/resources/UI.java
+++ src/edu/westga/cs4985/clinicApp/resources/UI.java
@@ -3,7 +3,10 @@ package edu.westga.cs4985.clinicApp.resources;
 public class UI {
 	
 	public static final class ExceptionMessages {
-
+		public static final String EMPTY_USERNAME = "Username cannot be empty";
+		public static final String NULL_USERNAME = "Username cannot be empty";
+		public static final String EMPTY_PASSWORD = "Password cannot be empty";
+		public static final String NULL_PASSWORD = "Password cannnot be null";
 	}
 
 }
