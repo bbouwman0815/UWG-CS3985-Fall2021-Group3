5c65eabfe7ef6a8fa6ff48e0ab0cb681305d52cb
Brian Bouwman <bbouwma1@my.westga.edu>
2021-08-31 18:09:29 -0400

    Changed project to Maven design

---
+++ src/main/java/edu/westga/cs4985/clinicApp/viewmodel/PatientGeneralInfomationViewModel.java
@@ -0,0 +1,21 @@
+package edu.westga.cs4985.clinicApp.viewmodel;
+
+/**
+ * The view model for patient general information.
+ * 
+ * @author Jinxiang Zeng
+ * @version Fall 2021
+ *
+ */
+public class PatientGeneralInfomationViewModel {
+	
+	/**
+	 * Create view model for patient general information.
+	 * 
+	 * @precondition none
+	 * 
+	 * @postcondition none
+	 */
+	public PatientGeneralInfomationViewModel() {
+	}
+}
