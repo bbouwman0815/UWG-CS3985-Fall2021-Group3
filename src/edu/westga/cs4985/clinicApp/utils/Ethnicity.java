6d361d3560bf98c3144a425202cc3a0dc99bc417
Jinxiang Zeng <jzeng1@my.westga.edu>
2021-08-30 21:29:16 -0400

    Add constant values for choices boxes
    I added constants values for choices box in general information, and the GUI should display values in choice boxes.

---
+++ src/edu/westga/cs4985/clinicApp/utils/Ethnicity.java
@@ -0,0 +1,14 @@
+package edu.westga.cs4985.clinicApp.utils;
+
+/**
+ * The Ethnicity list class.
+ * 
+ * @author Jinxiang Zeng
+ * @version Fall 2021
+ *
+ */
+public class Ethnicity {
+
+	public String[] ethnicity = {"Hispanic or Latino", "Not Hispanic or Latino"};
+
+}
