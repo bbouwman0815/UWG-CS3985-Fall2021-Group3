c83d468f107ac905da94709ae6c16b8c8806e6ea
Brian Bouwman <bbouwma1@my.westga.edu>
2021-09-02 14:17:31 -0400

    moved server

---
+++ Client/src/main/java/edu/westga/cs4985/clinicApp/utils/Race.java
@@ -0,0 +1,15 @@
+package edu.westga.cs4985.clinicApp.utils;
+
+/**
+ * The Race list class.
+ * 
+ * @author Jinxiang Zeng
+ * @version Fall 2021
+ *
+ */
+public class Race {
+	
+	public String[] race = {"American Indian or Alaska Native", "Asian", "Black or African American"
+							,"Native Hawaiian or Other Pacific Islander", "White"};
+
+}
---
+++ Client/src/main/java/edu/westga/cs4985/clinicApp/view/dashboard/DashboardCodeBehind.java
@@ -0,0 +1,57 @@
+package edu.westga.cs4985.clinicApp.view.dashboard;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class DashboardCodeBehind {
+
+	@FXML
+	private GridPane navbarGridPane;
+
+	@FXML
+	private Button generalInfoNavButton;
+
+	@FXML
+	private Button medicationsNavButton;
+
+	@FXML
+	private Button medicalConditionsNavButton;
+
+	@FXML
+	private Button appointmentNavButton;
+	
+    @FXML
+    private AnchorPane anchorPaneViewer;
+
+	public DashboardCodeBehind() {
+
+	}
+
+	@FXML
+	void handleNavigateAppointment(ActionEvent event) {
+
+	}
+
+	@FXML
+	void handleNavigateMedicalConditions(ActionEvent event) {
+
+	}
+
+	@FXML
+	void handleNavigateMedications(ActionEvent event) {
+
+	}
+
+	@FXML
+	void handleNavigateToGeneralInfo(ActionEvent event) throws IOException {
+		AnchorPane pane = FXMLLoader.load(getClass().getResource("../generalInfor/PatientGeneralInfoGui.fxml"));
+		this.anchorPaneViewer.getChildren().setAll(pane);
+	}
+
+}
