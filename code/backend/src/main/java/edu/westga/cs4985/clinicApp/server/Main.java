5db972acfa1abc7abd0691b88c73fe9b324d724c
Jinxiang Zeng <jzeng1@my.westga.edu>
2021-09-08 12:50:50 -0400

    Add server

---
+++ code/backend/src/main/java/edu/westga/cs4985/clinicApp/server/Main.java
@@ -0,0 +1,17 @@
+package edu.westga.cs4985.clinicApp.server;
+
+import org.zeromq.ZMQ;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Server server = new Server();
+		Client client = new Client();
+		
+		server.start();
+		client.start();
+		
+		System.out.println(ZMQ.CHARSET);
+	}
+
+}
