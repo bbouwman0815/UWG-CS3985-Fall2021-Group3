b2676938380087039785932c2d12af81cccff867
David Walker <thewalker.david@gmail.com>
2021-10-10 14:56:55 -0400

    Changed Birthday Validater

--- code/frontend/src/main/java/edu/westga/cs4985/clinicApp/resources/InputValidators.java
+++ code/frontend/src/main/java/edu/westga/cs4985/clinicApp/resources/InputValidators.java
@@ -41,7 +41,7 @@ public class InputValidators {
 	 * @return true, if successful
 	 */
 	public static boolean validateBirthday(String birthday) {
-		Pattern pattern = Pattern.compile("^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$");
+		Pattern pattern = Pattern.compile("^(\\d{4})-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$");
 		Matcher matcher = pattern.matcher(birthday);
 		return matcher.matches();
 	}
