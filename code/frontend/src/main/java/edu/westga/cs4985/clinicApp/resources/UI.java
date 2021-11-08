f86a6a5a9df719d50abc1e1f69a93126ba3e9d17
dwalke22 <thewalker.david@gmail.com>
2021-11-07 22:47:24 -0500

    Fixed UI checkstyles

--- code/frontend/src/main/java/edu/westga/cs4985/clinicApp/resources/UI.java
+++ code/frontend/src/main/java/edu/westga/cs4985/clinicApp/resources/UI.java
@@ -5,7 +5,7 @@ public class UI {
 	public static final class ExceptionMessages {
 		
 		public static final String EMPTY_USERNAME = "Username cannot be empty";
-		public static final String NULL_USERNAME = "Username cannot be empty";
+		public static final String NULL_USERNAME = "Username cannot be null";
 		public static final String EMPTY_PASSWORD = "Password cannot be empty";
 		public static final String NULL_PASSWORD = "Password cannnot be null";
 		
