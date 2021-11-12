d821c900d572be0debd835d7dbd84de1f2c10947
David Walker <thewalker.david@gmail.com>
2021-11-12 14:35:21 -0500

    Formatted Login Utils files

--- code/frontend/src/main/java/edu/westga/cs4985/clinicApp/utils/login/ILogin.java
+++ code/frontend/src/main/java/edu/westga/cs4985/clinicApp/utils/login/ILogin.java
@@ -9,18 +9,19 @@ import edu.westga.cs4985.clinicApp.model.User;
  * @version Fall 2021
  */
 public interface ILogin {
-	
+
 	/*
 	 * Validates a users credentials
 	 * 
 	 * @param username the username
+	 * 
 	 * @param password the password
 	 * 
 	 * @return true if valid, false otherwise
 	 * 
 	 */
 	boolean validateCredentials(String username, String password);
-	
+
 	/*
 	 * Logs the user in
 	 */
