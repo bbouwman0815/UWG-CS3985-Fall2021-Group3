994825997bd4422ab9a9f846f0f20b6153bc190e
bbouwman0815 <bbouwma1@my.westga.edu>
2021-11-14 09:27:43 -0500

    added test coverage for register caregiver gui - fixed general info gui tests that broke

--- code/frontend/src/test/java/edu/westga/cs4985/clinicApp/test/UITesting/TestUpdateGeneralInfo.java
+++ code/frontend/src/test/java/edu/westga/cs4985/clinicApp/test/UITesting/TestUpdateGeneralInfo.java
@@ -30,20 +30,23 @@ public class TestUpdateGeneralInfo extends ApplicationTest {
 
 			if (request.equals(
 					"USER_LOGIN,{\"password\":\"5f4dcc3b5aa765d61d8327deb882cf99\",\"userName\":\"bbouwman0815\"}")) {
-				return "{\"insurance\":\"United Healthcare\",\"lastName\":\"Bouwman\",\"country\":\"United States\",\"gender\":\"Male\",\"race\":\"White\",\"address2\":\"\",\"city\":\"Carrollton\",\"address1\":\"912 Lovvorn Road\",\"dateOfBirth\":\"2021-09-15\",\"type\":\"PATIENT\",\"userName\":\"bbouwman0815\",\"firstName\":\"Brian\",\"password\":\"5f4dcc3b5aa765d61d8327deb882cf99\",\"phoneNumber\":\"6665554444\",\"ethnicty\":\"Not Hispanic or Latino\",\"caregiver\":\"\",\"state\":\"GA\",\"email\":\"bbouwman0815@yahoo.com\"}";
+				return "{\"insurance\":\"United Healthcare\",\"lastName\":\"Bouwman\",\"country\":\"United States\",\"gender\":\"Male\",\"race\":\"White\",\"address2\":\"\",\"city\":\"Carrollton\",\"address1\":\"912 Lovvorn Road\",\"dateOfBirth\":\"2021-09-15\",\"type\":\"PATIENT\",\"userName\":\"bbouwman0815\",\"firstName\":\"Brian\",\"password\":\"5f4dcc3b5aa765d61d8327deb882cf99\",\"phoneNumber\":\"6665554444\",\"ethnicty\":\"Not Hispanic or Latino\",\"caregiver\":\"timi1\",\"state\":\"GA\",\"email\":\"bbouwman0815@yahoo.com\"}";
 			}
 			if (request.equals(
 					"ADD_MEDICAL_CONDITION,{\"terminationDate\":\"2016-11-01\",\"notes\":\"this disease lasts six years\",\"patient\":\"bbouwman0815\",\"diagnosisDate\":\"2010-11-01\",\"name\":\"sixyearsdiseaselmao\"}")) {
 				return "ADDED";
 			}
 			if (request.equals("GET_USER_BY_USERNAME,{\"patient\":\"bbouwman0815\"}")) {
-				return "{\"insurance\":\"United Healthcare\",\"lastName\":\"Bouwman\",\"country\":\"United States\",\"gender\":\"Male\",\"race\":\"White\",\"address2\":\"\",\"city\":\"Carrollton\",\"address1\":\"912 Lovvorn Road\",\"dateOfBirth\":\"2021-09-15\",\"type\":\"PATIENT\",\"userName\":\"bbouwman0815\",\"firstName\":\"Brian\",\"password\":\"5f4dcc3b5aa765d61d8327deb882cf99\",\"phoneNumber\":\"6665554444\",\"ethnicty\":\"Not Hispanic or Latino\",\"caregiver\":\"\",\"state\":\"GA\",\"email\":\"bbouwman0815@yahoo.com\"}";
+				return "{\"insurance\":\"United Healthcare\",\"lastName\":\"Bouwman\",\"country\":\"United States\",\"gender\":\"Male\",\"race\":\"White\",\"address2\":\"\",\"city\":\"Carrollton\",\"address1\":\"912 Lovvorn Road\",\"dateOfBirth\":\"2021-09-15\",\"type\":\"PATIENT\",\"userName\":\"bbouwman0815\",\"firstName\":\"Brian\",\"password\":\"5f4dcc3b5aa765d61d8327deb882cf99\",\"phoneNumber\":\"6665554444\",\"ethnicty\":\"Not Hispanic or Latino\",\"caregiver\":\"timi1\",\"state\":\"GA\",\"email\":\"bbouwman0815@yahoo.com\"}";
 			}
 			if (request.equals("GET_MEDICAL_CONDITIONS,{\"patient\":\"bbouwman0815\"}")) {
 				return "[{\"terminationDate\":\"2021-11-01\",\"notes\":\"this disease lasts six years lmao\",\"patient\":\"bbouwman0815\",\"diagnosisDate\":\"2010-11-01\",\"name\":\"sixyearsdiseaselmao\"}]";
 			}
 			if (request.equals("GET_ALL_CAREGIVERS,GET_ALL_CAREGIVERS")) {
 				return "[{\"lastName\":\"liu\",\"country\":\"Bhutan\",\"gender\":\"Male\",\"race\":\"Asian\",\"address2\":\"123\",\"city\":\"Carronllton\",\"address1\":\"1601 Maple St\",\"dateOfBirth\":\"1990-09-29\",\"type\":\"Caregiver\",\"userName\":\"timi1\",\"firstName\":\"timi\",\"password\":\"5f4dcc3b5aa765d61d8327deb882cf99\",\"phoneNumber\":\"4444444444\",\"ethnicty\":\"Hispanic or Latino\",\"state\":\"GA\",\"email\":\"temail@email\"}]";
+			}
+			if (request.equals("GET_CAREGIVER_BY_USER_NAME,{\"caregiver\":\"timi1\"}")) {
+				return "{\"lastName\":\"liu\",\"country\":\"Bhutan\",\"gender\":\"Male\",\"race\":\"Asian\",\"address2\":\"123\",\"city\":\"Carronllton\",\"address1\":\"1601 Maple St\",\"dateOfBirth\":\"1990-09-29\",\"type\":\"Caregiver\",\"userName\":\"timi1\",\"firstName\":\"timi\",\"password\":\"5f4dcc3b5aa765d61d8327deb882cf99\",\"phoneNumber\":\"4444444444\",\"ethnicty\":\"Hispanic or Latino\",\"state\":\"GA\",\"email\":\"temail@email\"}";
 			} else {
 				return "ERROR";
 			}
@@ -77,50 +80,18 @@ public class TestUpdateGeneralInfo extends ApplicationTest {
 		this.type(KeyCode.D);
 		this.clickOn("#loginButton");
 		this.clickOn("#generalInfoNavButton");
-		this.clickOn("#editbutton");
-		this.clickOn("#addCaregiverButton");
-		this.clickOn("#caregiverList");
-		this.type(KeyCode.UP);
-		this.type(KeyCode.ENTER);
 		this.type(KeyCode.TAB);
-		this.type(KeyCode.ENTER);
-
-	}
-
-	@Test
-	public void testUpdateRemoveCareGiver() throws InterruptedException {
-		this.clickOn("#usernameTextField");
-		this.type(KeyCode.B);
-		this.type(KeyCode.B);
-		this.type(KeyCode.O);
-		this.type(KeyCode.U);
-		this.type(KeyCode.W);
-		this.type(KeyCode.M);
-		this.type(KeyCode.A);
-		this.type(KeyCode.N);
-		this.type(KeyCode.DIGIT0);
-		this.type(KeyCode.DIGIT8);
-		this.type(KeyCode.DIGIT1);
-		this.type(KeyCode.DIGIT5);
-		this.clickOn("#passwordTextField");
-		this.type(KeyCode.P);
-		this.type(KeyCode.A);
-		this.type(KeyCode.S);
-		this.type(KeyCode.S);
-		this.type(KeyCode.W);
-		this.type(KeyCode.O);
-		this.type(KeyCode.R);
-		this.type(KeyCode.D);
-		this.clickOn("#loginButton");
-		this.clickOn("#generalInfoNavButton");
-		this.clickOn("#editbutton");
-		this.clickOn("#addCaregiverButton");
-		this.clickOn("#caregiverList");
-		this.type(KeyCode.UP);
-		this.type(KeyCode.ENTER);
 		this.type(KeyCode.TAB);
-		this.type(KeyCode.ENTER);
+		this.type(KeyCode.TAB);
+		this.clickOn("#editbutton");
 		this.clickOn("#removeCaregiverButton");
+		this.clickOn("#addCaregiverButton");
+		this.clickOn("#caregiverList");
+		this.type(KeyCode.UP);
+		this.type(KeyCode.ENTER);
+		this.type(KeyCode.TAB);
+		this.type(KeyCode.ENTER);
+
 	}
 
 	@Test
@@ -150,7 +121,11 @@ public class TestUpdateGeneralInfo extends ApplicationTest {
 		this.clickOn("#loginButton");
 		this.clickOn("#generalInfoNavButton");
 		this.clickOn("#editbutton");
+		this.clickOn("#removeCaregiverButton");
 		this.clickOn("#addCaregiverButton");
+		this.type(KeyCode.TAB);
+		this.type(KeyCode.TAB);
+		this.type(KeyCode.ENTER);
 	}
 
 	@Test
@@ -180,6 +155,7 @@ public class TestUpdateGeneralInfo extends ApplicationTest {
 		this.clickOn("#loginButton");
 		this.clickOn("#generalInfoNavButton");
 		this.clickOn("#editbutton");
+		this.clickOn("#removeCaregiverButton");
 		this.clickOn("#addCaregiverButton");
 		this.clickOn("#caregiverList");
 		this.type(KeyCode.TAB);
